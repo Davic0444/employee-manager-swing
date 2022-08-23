e4a904187dab9ca374526ceddcebeec17af63931
Davic0444 <34513322+Davic0444@users.noreply.github.com>
2022-08-24 00:26:45 +0200

    Create employee model and database connection.

---
+++ src/main/java/com/david/model/Employee.java
@@ -0,0 +1,98 @@
+package main.java.com.david.model;
+
+import java.time.LocalDate;
+
+public class Employee {
+
+    private int id;
+    private String firstName;
+    private String lastName;
+    private LocalDate birthOfDate;
+    private String identityCard;
+    private int salary;
+    private int employeeCategoryId;
+    private boolean status;
+    private boolean deleted;
+
+    public Employee(int id, String firstName, String lastName, LocalDate birthOfDate, String identityCard, int salary, int employeeCategoryId, boolean status, boolean deleted) {
+        this.id = id;
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.birthOfDate = birthOfDate;
+        this.identityCard = identityCard;
+        this.salary = salary;
+        this.employeeCategoryId = employeeCategoryId;
+        this.status = status;
+        this.deleted = deleted;
+    }
+
+    public Employee(String firstName, String lastName, LocalDate birthOfDate, String identityCard, int salary, int employeeCategoryId, boolean status, boolean deleted) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.birthOfDate = birthOfDate;
+        this.identityCard = identityCard;
+        this.salary = salary;
+        this.employeeCategoryId = employeeCategoryId;
+        this.status = status;
+        this.deleted = deleted;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public LocalDate getBirthOfDate() {
+        return birthOfDate;
+    }
+
+    public String getIdentityCard() {
+        return identityCard;
+    }
+
+    public int getSalary() {
+        return salary;
+    }
+
+    public int getEmployeeCategoryId() {
+        return employeeCategoryId;
+    }
+
+    public boolean isStatus() {
+        return status;
+    }
+
+    public boolean isDeleted() {
+        return deleted;
+    }
+
+    public String getStatusText() {
+        String status = "aktív";
+        if (!this.status) {
+            status = "inaktív";
+        }
+        return status;
+    }
+
+    @Override
+    public String toString() {
+        return "Employee{" +
+                "id=" + id +
+                ", firstName='" + firstName + '\'' +
+                ", lastName='" + lastName + '\'' +
+                ", birthOfDate=" + birthOfDate +
+                ", identityCard='" + identityCard + '\'' +
+                ", salary=" + salary +
+                ", employeeCategoryId=" + employeeCategoryId +
+                ", status=" + status +
+                ", deleted=" + deleted +
+                '}';
+    }
+}
---
+++ src/main/java/com/david/model/EmployeeCategory.java
@@ -0,0 +1,42 @@
+package main.java.com.david.model;
+
+public class EmployeeCategory {
+
+    private int id;
+    private String name;
+    private boolean status;
+    private boolean deleted;
+
+    public EmployeeCategory(int id, String name, boolean status, boolean deleted) {
+        this.id = id;
+        this.name = name;
+        this.status = status;
+        this.deleted = deleted;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public boolean isStatus() {
+        return status;
+    }
+
+    public boolean isDeleted() {
+        return deleted;
+    }
+
+    @Override
+    public String toString() {
+        return "EmployeeCategory{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                ", status=" + status +
+                ", deleted=" + deleted +
+                '}';
+    }
+}
